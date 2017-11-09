82805939baa3338715291b420101ad75cc8876a8
ezeperezcrack <ezequielda.perez@outlook.com>
2017-11-09 16:48:59 -0300

    Update Cliente.ts
    falta compilar convertir a json y probar en casa

--- TpEstacionamiento/FrontEnd/Cliente.ts
+++ TpEstacionamiento/FrontEnd/Cliente.ts
@@ -5,11 +5,15 @@ namespace Estacionamiento{
 			marca:string;
 			color:string;
 			patente:string;
-			discapacitado:boolean;
-		constructor(mar:string,col:string,pat:string){
+			prioridad:boolean;
+		constructor(nombre:string,ape:string,mar:string,col:string,pat:string, prior:boolean){
+			
+			this.nombre=nombre;
+			this.apellido=ape;
 			this.marca = mar;
 			this.color = col;
 			this.patente = pat;
+			this.prioridad=prior;
 		}
 	}
 }
