5bee860f582a01f879f20382f8802a3d823ca7aa
ezeperezcrack <ezequielda.perez@outlook.com>
2017-11-20 12:39:36 -0300

    Add files via upload

--- TpEstacionamiento/BackEnd/UsuarioDB.php
+++ TpEstacionamiento/BackEnd/UsuarioDB.php
@@ -1,14 +1,29 @@
 <?php
-
+include_once "./AccesoDatos.php";
 class UsuarioDB{
 
     public $nombre;
     public $apellido;
-    public $edad;
-    public $email;
-    public $contraseña;
+    public $mail;
+    public $clave;
     public $turno;
-    public $ingreso;
+    public $perfil;
 
+    public function altaUsuario()
+	{
+			   $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			   $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into empleados (nombre,apellido,mail,clave,turno,perfil)values(:nombre,:apellido,:mail,:clave,:turno,:perfil)");
+			   $consulta->bindValue(':nombre',$this->nombre, PDO::PARAM_STR);
+			   $consulta->bindValue(':apellido', $this->apellido, PDO::PARAM_STR);
+			   $consulta->bindValue(':mail', $this->mail, PDO::PARAM_STR);
+			   $consulta->bindValue(':clave', $this->clave, PDO::PARAM_STR);
+               $consulta->bindValue(':turno', $this->turno, PDO::PARAM_STR);
+               $consulta->bindValue(':perfil', $this->perfil, PDO::PARAM_STR);
+               
+			   
+			   
+			   $consulta->execute();		
+			   return $objetoAccesoDato->RetornarUltimoIdInsertado();
+	}
 
 }
